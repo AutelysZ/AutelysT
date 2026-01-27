5330b999f95845cd6bcd3c88d6b6f12cf049b22b
autelysz <autelys@gmail.com>
2026-01-26 23:53:50 -0800

    docs

--- app/tools/x509/page.tsx
+++ app/tools/x509/page.tsx
@@ -1,11 +1,5 @@
 import X509Client from "./x509-client";
 
-export default function X509Page({
-  searchParams,
-}: {
-  searchParams?: Record<string, string | string[] | undefined>;
-}) {
-  const tabParam = searchParams?.tab;
-  const initialTab = typeof tabParam === "string" ? tabParam : undefined;
-  return <X509Client initialTab={initialTab} />;
+export default function X509Page() {
+  return <X509Client />;
 }
